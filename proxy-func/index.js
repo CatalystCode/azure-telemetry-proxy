6dd66b27658b5e468f051fcfa77489e3aafe0b8f
morsh <mor.shemesh@gmail.com>
2016-07-06 11:40:13 +0300

    adding function to push data to AA

---
+++ proxy-func/index.js
@@ -0,0 +1,12 @@
+var appInsights = require("applicationinsights");
+// Connecting to application insights
+var instrumentationKey = process.env.AppInsightInstrumentationKey;
+var client = appInsights.getClient(instrumentationKey);
+module.exports = function (context, message) {
+    // Sending all data to application insight analytics
+    context.log('message:', message);
+    client.trackEvent("telemetry event", message);
+    // Later... Send data to mongodb as well
+    // Inform function completion
+    context.done();
+};
---
+++ proxy-func/index.ts
@@ -0,0 +1,18 @@
+import appInsights = require("applicationinsights");
+
+// Connecting to application insights
+var instrumentationKey = process.env.AppInsightInstrumentationKey;
+var client = appInsights.getClient(instrumentationKey);
+
+// Exporting like this makes sure the JS output is in the format azure function expects it
+export = function (context, message) {
+
+  // Sending all data to application insight analytics
+  context.log('message:', message);
+  client.trackEvent("telemetry event", message);
+
+  // Later... Send data to mongodb as well
+
+  // Inform function completion
+  context.done();
+};
